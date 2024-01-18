822a0a489a2ecc0ae7ade3e73954c4e7d75eb280
Vitor <vitor.scastro2013@hotmail.com>
2024-01-18 16:51:56 -0300

    Primerio exercicio

---
+++ Jogos/adivinhacao.py
@@ -0,0 +1,18 @@
+print('*********************************')
+print('Bem vindo ao jogo de adivinhação!')
+print('*********************************')
+
+numero_secreto = 42
+
+numero_usuario = int(input('Digite o seu numero:'))
+
+if(numero_secreto == numero_usuario):
+    print('ACERTOU!')
+else:
+    print('Numero errado!')
+    
+
+
+
+
+    

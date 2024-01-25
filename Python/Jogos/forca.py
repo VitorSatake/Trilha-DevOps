0e3161dfb6d42fe15b4318859d5a229375c4ab48
Vitor <vitor.scastro2013@hotmail.com>
2024-01-25 16:49:31 -0300

    continuando com o curso

--- Python/Jogos/forca.py
+++ Python/Jogos/forca.py
@@ -11,11 +11,12 @@ def jogar():
     while(not enforcou and not acertou): 
         
         chute = input("Qual a letra? ")
+        chute = chute.strip()
         
         index = 0
         
         for letra in palavra_secreta:
-            if (chute == letra):
+            if (chute.upper() == letra.upper()):
                 print("Encontrei a letra {} na posição {}.".format(chute, index))
             index = index + 1
                 

2b9e2a67f36666d4373fb4ebe95a576a06e07d8a
Vitor <vitor.scastro2013@hotmail.com>
2024-02-04 19:21:46 -0300

    treinando rotas na api

--- Python/API/ambiente/main.py
+++ Python/API/ambiente/main.py
@@ -7,4 +7,10 @@ app = FastAPI()
 def hello_root():
     return {"message": "Hello World"}
 
+@app.get("/teste")
+
+def hello_teste():
+    return {"message": "Testando rotas"}
+
+
 #uvicorn ambiente.main:app --reload // comando para rodar o ambiente
